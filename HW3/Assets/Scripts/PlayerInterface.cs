554ab524bc7c023689862d21b6571e394833f012
Lily Ready (RIT Student) <lr4631@rit.edu>
2024-03-28 16:46:24 -0400

    continued work

--- HW3/Assets/Scripts/PlayerInterface.cs
+++ HW3/Assets/Scripts/PlayerInterface.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using RPS;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
 
 /// <summary>
 /// This class receives input from the player, queries the AI for predictions, and updates the total wins/losses.
@@ -14,6 +17,62 @@ public class PlayerInterface : MonoBehaviour
 
     // Records the number of times the AI has won.
     private int aiWins = 0;
+
+    List<RPSMove> sequence = new List<RPSMove>();
+
+    class KeyDataRecord
+    {
+        public Hashtable counts = new Hashtable();
+        public int total = 0;
+
+        public KeyDataRecord() 
+        {
+            counts.Add(0, 0);
+            counts.Add(1, 0);
+            counts.Add(2, 0);
+        }
+    }
+
+    static class NGramPredictor
+    {
+        private static Hashtable data = new Hashtable();
+        private static int nValue = Screen.width + 1;
+
+        public static void registerSequence(List<RPSMove> actions)
+        {
+            RPSMove[] key = actions.GetRange(0,nValue).ToArray();
+            RPSMove value = actions[nValue];
+            KeyDataRecord keyData;
+            if (!data.ContainsKey(key))
+            {
+                data.Add(key, new KeyDataRecord());
+            }
+            keyData = (KeyDataRecord)data[key];
+            if (value == RPSMove.Rock) keyData.counts[0] = (int)keyData.counts[0] + 1;
+            else if (value == RPSMove.Paper) keyData.counts[1] = (int)keyData.counts[1] + 1;
+            else if (value == RPSMove.Scissors) keyData.counts[1] = (int)keyData.counts[1] + 1;
+            keyData.total += 1;
+
+        }
+
+        public static RPSMove getMostLikely(RPSMove[] actions)
+        {
+            KeyDataRecord keyData = data[actions] as KeyDataRecord;
+            int highestValue = 0;
+            RPSMove bestAction = 0;
+
+            actions = keyData.counts.Keys as RPSMove[];
+            foreach (RPSMove action in actions)
+            {
+                if ((int)keyData.counts[action] > highestValue)
+                {
+                    highestValue = (int)keyData.counts[action];
+                    bestAction = action;
+                }
+            }
+            return bestAction;
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -35,11 +94,24 @@ public class PlayerInterface : MonoBehaviour
                 string output = "You selected: " + hit.collider.gameObject.name;
 
                 // Convert the collider clicked by the user to the r/p/s character in the input variable.
-                if (hit.collider.gameObject.name == "Rock") input = 'r';
-                else if (hit.collider.gameObject.name == "Paper") input = 'p';
-                else if (hit.collider.gameObject.name == "Scissors") input = 's';
+                if (hit.collider.gameObject.name == "Rock")
+                {
+                    input = 'r';
+                    sequence.Add(RPSMove.Rock);
+                }
+                else if (hit.collider.gameObject.name == "Paper")
+                {
+                    input = 'p';
+                    sequence.Add(RPSMove.Paper);
+                }
+                else if (hit.collider.gameObject.name == "Scissors")
+                {
+                    input = 's';
+                    sequence.Add(RPSMove.Scissors);
+                }
                 else return;
 
+                NGramPredictor.registerSequence(sequence);
                 // Ask the ngram AI to predict what the player will choose..
                 // You will need to implement this code and any history tracking it requires.
                 // For now, we will predict a move at random.
